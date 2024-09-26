0c31a853cb3dd7f0781ce02db6487ed23f14c3af
jsablaon <jsablaon@gmail.com>
2024-09-25 18:21:57 -0700

    added navigation login flow and main flow and added more stuff to sign up screen

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,10 +1,36 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { Text, Input, Button } from "react-native-elements";
+import Spacer from "../components/Spacer";
 
-const SignupScreen = () => {
-    return <View><Text style={{fontSize:48}}>SignupScreen</Text></View>
+const SignupScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Spacer>
+        <Text h3>Sign up for Tracker</Text>
+      </Spacer>
+      <Input label="Email" />
+      <Spacer />
+      <Input label="Email" />
+      <Spacer>
+        <Button title="Sign Up" />
+      </Spacer>
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+SignupScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 250,
+  },
+});
 
 export default SignupScreen;
